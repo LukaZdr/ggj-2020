75e231712ab75f6ae9b00bf6c7fe53edc2ae52bf
7reinhar <56921732+7reinhar@users.noreply.github.com>
2020-02-01 14:35:58 +0100

    Hole filling logic😏

---
+++ Assets/Scripts/Aufplatzring.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aufplatzring : MonoBehaviour
+{
+    [SerializeField]
+    GameObject boltPlaceholder;
+    [SerializeField]
+    float deleteDelay = 15f;
+
+    // Im Erfolgsfall ist other = Bolzen
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bolt")
+        {
+            Destroy(other.gameObject);
+            boltPlaceholder.SetActive(true);
+            this.GetComponent<Collider>().enabled = false;
+            Destroy(this.gameObject, deleteDelay);
+        }
+    }
+}
