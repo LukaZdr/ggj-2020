abea1e84cd3acf6e54832ece02a6868e790ac1c3
7reinhar <56921732+7reinhar@users.noreply.github.com>
2020-02-01 12:11:49 +0100

    Make shovel great again

---
+++ Assets/Scripts/DualHand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class DualHand : MonoBehaviour
+{
+    private Interactable interactable;
+    private Hand hand;
+    private Transform secondHand;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void OnAttachedToHand(Hand attachedHand)
+    {
+        hand = attachedHand;
+        secondHand = hand.otherHand.transform;
+        hand.otherHand.enabled = false;
+    }
+
+    private void OnDetachedFromHand(Hand hand)
+    {
+        hand.otherHand.enabled = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (interactable.attachedToHand)
+        {
+            interactable.transform.position = hand.transform.position;
+            interactable.transform.LookAt(secondHand);
+        }
+    }
+}
