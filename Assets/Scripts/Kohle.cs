b4a236c312c8377bfba0cf69ab03c9b81c78f01d
7reinhar <56921732+7reinhar@users.noreply.github.com>
2020-02-01 20:05:03 +0100

    adds kohlestücke

---
+++ Assets/Scripts/Kohle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kohle : MonoBehaviour
+{
+    // Im Erfolgsfall ist other = Ofen
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ofen")
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
