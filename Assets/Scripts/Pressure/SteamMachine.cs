16256aac3074c3f14ba840470a38fd9d82f2d633
7reinhar <56921732+7reinhar@users.noreply.github.com>
2020-02-02 12:39:37 +0100

    implement steamMachine

---
+++ Assets/Scripts/Pressure/SteamMachine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pressure
+{
+    public class SteamMachine : VoidSink
+    {
+        [Tooltip("How much steam this machine draws at maximum (in liters per second)")]
+        public float maxDraw = 5;
+        [Tooltip("How much steam this machine draws at minimum (in liters per second)")]
+        public float minDraw = 0.5f;
+
+        public void FixedUpdate()
+        {
+            maxInputCapacity = Mathf.Min(maxDraw, maxInputCapacity + Random.Range(-0.1f, 0.1f));
+        }
+    }
+}
